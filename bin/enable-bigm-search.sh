f45d617703523f4dc4d0917caf706a2a49a80309
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2026-09-21 19:27:07 +0900

    update

--- bin/enable-bigm-search.sh
+++ bin/enable-bigm-search.sh
@@ -6,8 +6,9 @@ set -euCo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" 2>&1 >/dev/null && pwd)"
 export COMPOSE_PROJECT_DIR="${COMPOSE_PROJECT_DIR:-"${SCRIPT_DIR}"/..}"
 export COMPOSE_FILE="$SCRIPT_DIR"/../docker-compose.yml
+. "$SCRIPT_DIR"/../etc/docker.env
 
-docker compose exec db psql -d misskey -c "
+docker compose exec db psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
   create index concurrently if not exists
     note_lower_text_bigm
   on note
